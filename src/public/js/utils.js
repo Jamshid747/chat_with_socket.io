4fe92ee613dc61a20841256eb9d0cef831e2a8e3
Jamshid Maxmudov <jamshid.qwerty@mail.ru>
2022-07-25 15:59:29 +0500

    frontend -login -register finished

---
+++ src/public/js/utils.js
@@ -0,0 +1,24 @@
+const request = async (rout, method, body) => {
+    const headers = {
+        token: window.localStorage.getItem('token')
+    }
+    if(!(body instanceof FormData)) {
+        headers['Content-Type'] = 'application/json'
+        body = JSON.stringify(body || null)
+    }
+    let response = await fetch(rout, {
+        method,
+        headers,
+        body
+    })
+    response = await response.json()
+    
+    if(response.status == 403) {
+        window.localStorage.clear()
+        window.location - '/login'
+    } else if(response.status > 399) {
+        errorMessage.textContent = response.message
+    } else {
+        return response
+    }
+}
