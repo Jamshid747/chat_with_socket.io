09e7ece33810455645c4c02f22d712d0a7750a2b
Jamshid Maxmudov <jamshid.qwerty@mail.ru>
2022-07-25 14:03:32 +0500

    -extra finished

---
+++ src/modules/extra/index.js
@@ -0,0 +1,13 @@
+import checkToken from "../../middlewares/checkToken.js"
+import { Router } from "express"
+import CT from "./controller.js"
+
+const router = Router()
+
+router.get('/profileAvatar/:token', checkToken, CT.GET_AVATAR)
+router.get('/profileUsername/:token', checkToken, CT.GET_USERSNAME)
+router.get('/file/:token/:fileName', checkToken, CT.GET_FILE)
+router.get('/download/:token/:fileName', checkToken, CT.DOWNLOAD_FILE)
+
+
+export default router
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 import HomeRouter from "./home/index.js"
 import UserRouter from "./user/index.js"
 import MessageRouter from "./message/index.js"
+import ExraRouter from "./extra/index.js"
 
 export default [
     MessageRouter,
+    ExraRouter,
     HomeRouter,
     UserRouter
 ]
